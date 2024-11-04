4e58e27b3a076b112d0b106b458bbc72e177756c
Luana izy Veras Tavares <luanaizy@gmail.com>
2024-11-04 17:40:06 -0300

    first component

---
+++ src/components/FruitItem.tsx
@@ -0,0 +1,12 @@
+const FruitItem = () => {
+
+    return(
+        <div>
+            <h2>nome</h2>
+            <h4>preco</h4>
+            <p>lote</p>
+        </div>
+    )
+
+}
+export default FruitItem
